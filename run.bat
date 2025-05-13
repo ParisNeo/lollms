551e7437ea0f780296f9399a67b2753c40406c1f
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2025-05-13 06:52:36 +0200

    fix

--- run.bat
+++ run.bat
@@ -1,15 +1,16 @@
 @echo off
 setlocal
-
 :: Check if venv exists
 if not exist "venv\Scripts\activate.bat" (
     echo Virtual environment not found. Please run install.bat first.
     exit /b 1
 )
 
+echo welcome to simplified lollms
 :: Activate virtual environment
-call venv\Scripts\activate.bat
+call .\venv\Scripts\activate.bat
 
+echo Running server
 :: Run the app
 python main.py
 
