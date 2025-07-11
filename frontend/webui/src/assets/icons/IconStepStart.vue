dfe1a0dcb5ca2bc8bf66fa377fc58f5576a5fff0
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2025-07-11 12:31:16 +0200

    Create IconStepStart.vue

---
+++ frontend/webui/src/assets/icons/IconStepStart.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+    <path d="M6.3 2.84A1.5 1.5 0 0 0 4 4.11v11.78a1.5 1.5 0 0 0 2.3 1.27l9.344-5.89a1.5 1.5 0 0 0 0-2.54L6.3 2.84Z" />
+  </svg>
+</template>
