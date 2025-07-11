6650e0dfd512317666824a8182f91ffe46bf7b93
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2025-07-11 12:26:05 +0200

    Create IconScratchpad.vue

---
+++ frontend/webui/src/assets/icons/IconScratchpad.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-full">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931z" />
+    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 7.125l-8.932 8.931m0 0L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931z" />
+  </svg>
+</template>
