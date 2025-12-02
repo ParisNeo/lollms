a44c297083bc0a17d7696316afdcc5d6e4a01bf0
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2025-12-02 01:23:07 +0100

    added new lollms postng capabilities

---
+++ frontend/dist/assets/clone-BVC7X_sC.js
@@ -0,0 +1,1 @@
+import{b as r}from"./_baseUniq-C8C7z_o3.js";var e=4;function a(o){return r(o,e)}export{a as c};
--- frontend/dist/assets/clone-vVb1Biw_.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./_baseUniq-FymEmn6J.js";var e=4;function a(o){return r(o,e)}export{a as c};
